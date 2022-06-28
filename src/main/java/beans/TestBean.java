5c8a7fb7c3c88ca3957ed6092bdef2a8b2caceb5
alsgp0877 <alsgp0877@naver.com>
2022-06-28 23:48:13 +0900

    ioc컨테이너 공부

---
+++ src/main/java/beans/TestBean.java
@@ -0,0 +1,10 @@
+package beans;
+
+public class TestBean{
+
+	public TestBean() {
+		System.out.println("TestBean생성자");
+	}
+
+	
+}
--- src/main/java/main/MainClass.java
+++ src/main/java/main/MainClass.java
@@ -1,24 +1,65 @@
 package main;
 
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 
 import beans.HelloWorld;
+import beans.TestBean;
 
 public class MainClass {
 	
 	public static void main(String[] args) {
 		
-		//beans.xml 파일을 로딩한다.
+		test1();
+		test2();
+		test3();
+		test4();
+	}
+	
+	//스프링에서 제공하는 함수
+	//ClassPathResource jar,war 내에서 찾는다. 주로 src/main/resources아래 파일을을 사용하는데 이용된다.
+	//FileSystemResource 실제 파일 시스템 내에서 파일을 찾는다. 
+	public static void test1(){
+		//xml 파일만 생성하고 객체 생성안한다.
+		ClassPathResource res = new ClassPathResource("config/beans.xml");
+		XmlBeanFactory facotry = new XmlBeanFactory(res);
+
+		//이렇게 따로 생성하고 주소가 만들어진다. 
+		TestBean t1 = facotry.getBean("t1",TestBean.class);
+		System.out.printf("t1:%s\n", t1);
+	}
+	
+	public static void test2() {
+		FileSystemResource res = new FileSystemResource("beans.xml");
+		XmlBeanFactory facotry = new XmlBeanFactory(res);
+
+		TestBean t1 = facotry.getBean("t1",TestBean.class);
+		System.out.printf("t1:%s\n", t1);
+	}
+	
+	//패키지내부
+	public static void test3() {
+		//따로 설정하지 않으면 bean.xml에 설정되있는 bean 객체들이 자동으로 다 생성이된다.
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config/beans.xml");
+		//이미 만들어져 있는 주소값의 객체를 받아서 사용하는거다.
+		TestBean t1 = ctx.getBean("t1",TestBean.class);
+		System.out.printf("t1:%s\n", t1);
 		
-		//xml에 정의한 bean 객체의 주소값을 가져온다.
-		HelloWorld hello1 = (HelloWorld) ctx.getBean("hello");
-		callMethod(hello1);
 		ctx.close();
 	}
 	
-	public static void callMethod(HelloWorld hello) {
-		hello.sayHello();
+	//패키지외부
+	public static void test4() {
+		//따로 설정하지 않으면 bean.xml에 설정되있는 bean 객체들이 자동으로 다 생성이된다.
+		FileSystemXmlApplicationContext ctx = new FileSystemXmlApplicationContext("beans.xml");
+		//이미 만들어져 있는 주소값의 객체를 받아서 사용하는거다.
+		TestBean t1 = ctx.getBean("t1",TestBean.class);
+		System.out.printf("t1:%s\n", t1);
+		
+		ctx.close();
 	}
 
 }
